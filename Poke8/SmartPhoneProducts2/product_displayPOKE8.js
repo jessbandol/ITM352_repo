e0c69be898236d971212f107bcd16318a7fea3e3
jessbandol <jmbandol@hawaii.edu>
2023-10-15 22:27:50 -1000

    update poke8

--- Poke8/SmartPhoneProducts2/product_displayPOKE8.js
+++ Poke8/SmartPhoneProducts2/product_displayPOKE8.js
@@ -1,7 +1,10 @@
-//declare and push to the DOM the store name at top and bottom
-const store_name="Bandol";
-top_title.innerHTML=(store_name + "'s Used Smart Phone Store");
-bottom_title.innerHTML=(store_name + "'s Used Smart Phone Store");
+//declare store name
+const first_name="Jessica";
+const last_name ="Bandol's";
+const store_name ="Bandol";
+
+const top_title = document.getElementById('top_title');
+top_title.innerHTML = (store_name + "'s Used Smart Phone Store");
 
 let hits= 0;
 let spins=0;
@@ -9,6 +12,46 @@ let spins=0;
 let over_half=false;
 hits_span.innerHTML = hits; 
 spins_span.innerHTML = spins;
+
+//define variables
+let name1 = "HTC";
+let price1 = 40.00;
+let image1 = "Images/HTC.jpg";
+
+let name2 = "Apple";
+let price2 = 75.00;
+let image2 = "Images/iphone-3gs.jpg";
+
+let name3 = "Nokia";
+let price3 = 35.00;
+let image3 = "Images/Nokia.jpg";
+
+let name4 = "Samsung";
+let price4 = 45.00;
+let image4 = "Images/Samsung.jpg";
+
+let name5 = "Blackberry";
+let price5 = 10.00;
+let image5 = "Images/Blackberry.jpg";
+
+function appendImageToContainer(imagePath) {
+    let imgElement = document.createElement("img");
+    document.getElementById("imageContainer").appendChild(imgElement);
+}
+appendImageToContainer(image1);
+
+for (let i=1; eval("typeof name" + i) != 'undefined'; i++) {
+    document.querySelector('.main').innerHTML += `
+    <section class ="item" onmouseover="changeClassName(this);"onclick="resetClassName(this);">
+    <h2>${eval("name"+i)}</h2>
+    <p>$${eval("price"+i)}</p>
+    <img src="${eval("image"+i)}"/>
+    </section>`;
+}
+
+//DOM for current year and time
+const currentYear = new Date ().getFullYear();
+const currentTime = new Date ().toLocaleTimeString([], {hour: '2-digit'});
 
 function changeClassName(element) {
     if(element.className=='item'){
@@ -31,20 +74,6 @@ function changeClassName(element) {
     // -- Winning progress depends on hits/spins
     let hits_spins_ratio = hits/spins;
     let progress
-    /*if ( hits_spins_ratio > 0 ) {
-        progress = 'On your way!';
-        if ( hits_spins_ratio >= 0.25 ) {
-            progress = 'Almost there!';
-            if ( hits_spins_ratio >= 0.5 ) {
-                if( hits < spins) { 
-                    progress = 'You win!';
-                }
-            }
-        }
-    }
-    else {
-        progress = 'Get going!' ;
-    }*/
 
     if(hits_spins_ratio >= 0.5 && hits<spins) {
             progress='You win!';
@@ -86,20 +115,6 @@ function resetClassName(element) {
     // -- Winning progress depends on hits/spins
     let hits_spins_ratio = hits/spins;
     let progress
-    /*if ( hits_spins_ratio > 0 ) {
-        progress = 'On your way!';
-        if ( hits_spins_ratio >= 0.25 ) {
-            progress = 'Almost there!';
-            if ( hits_spins_ratio >= 0.5 ) {
-                if( hits < spins) { 
-                    progress = 'You win!';
-                }
-            }
-        }
-    }
-    else {
-        progress = 'Get going!' ;
-    }*/
 
     if(hits_spins_ratio >= 0.5 && hits<spins) {
             progress='You win!';
@@ -113,3 +128,30 @@ function resetClassName(element) {
     }
     win_span.innerHTML=progress;
 }
+
+//footer
+const footerTable = `
+<table>
+    <tr>
+        <td></td>
+        <td class=“table-header”>Your One Stop Shop For Used Phones - ${first_name.charAt(0).toUpperCase()}.${last_name.charAt(0).toUpperCase()}’s</td>
+    </tr>
+
+    <tr>
+        <td>1.</td>
+        <td>Copyright @ ${first_name} ${last_name}</td>
+    </tr>
+    
+    <tr>
+        <td>2.</td>
+        <td>${currentYear}</td>
+    </tr>   
+    
+    <tr>
+        <td>3.</td>
+        <td>${currentTime}</td>
+    </tr>
+</table>
+`;
+
+bottom_title.innerHTML = footerTable;
