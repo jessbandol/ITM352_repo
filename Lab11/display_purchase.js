d7146f606aa7ea73cfd94291d0f5a7f81397fb24
jessbandol <jmbandol@hawaii.edu>
2023-10-28 17:37:40 -1000

    update lab11

--- Lab11/display_purchase.js
+++ Lab11/display_purchase.js
@@ -1,22 +1,5 @@
 let params = (new URL(document.location)).searchParams;
+let quantity=params.get("qty_textbox");
 
-if (params.has("qty_textbox")) {
-    let quantity = params.get("qty_textbox");
-    
-    if (isValidNonNegativeInteger(quantity)) {
-        document.body.innerHTML = `Thank you for ordering ${quantity} things!`;
-    } else {
-        document.body.innerHTML = "Invalid quantity. Press the back button and try again.";
-    }
-} else {
-    document.body.innerHTML = "Quantity not provided. Please go back and enter a valid quantity.";
-}
-
-function isValidNonNegativeInteger(q, returnErrors = false) {
-    let errors = [];
-    if (Number(q) != q) errors.push('Not a number!');
-    if (q < 0) errors.push('Negative value!');
-    if (parseInt(q) != q) errors.push('Not an integer!');
-    
-    return returnErrors ? errors : (errors.length == 0);
-}
+let message = `Thank you for ordering ${quantity} things!`;
+document.body.innerHTML=message;
--- Lab11/order3.js
+++ Lab11/order3.js
@@ -1,6 +1,50 @@
-function updateMessage(theTextBox) {
-    let messageElement = document.getElementById(`${theTextBox.name}_message`);
-    // Here, replace theTextBox.id with theTextBox.name
-    messageElement.innerHTML = theTextBox.value;
-    // ... rest of your validation logic
+function updateQuantityMessage(textbox) {
+    let quantityMessage=document.getElementById('qty_textbox_message');
+    
+    //validate quantity
+    let validationMessage=validateQuantity(Number(textbox.value));
+
+    //display error message
+    if(validationMessage !=="") {
+        quantityMessage.innerHTML = validationMessage;
+    } else {
+        quantityMessage.innerHTML = textbox.value;
+    } 
+}
+
+function validateQuantity(quantity) {
+    let errorMessage = "";
+
+    switch (true) {
+        case isNaN(quantity):
+            errorMessage= "Not a number. Please enter a non-negative quantity to order.";
+            break;
+        case quantity <= 0 && !Number.isInteger(quantity):
+            errorMessage = "Negative inventory and not an Integer. Please enter a non-negtive quantity to order.";
+            break;
+        case quantity <= 0:
+            errorMessage = "Negative inventory. Please enter a non-negative quantity to order.";
+            break;
+        case !Number.isInteger(quantity):
+            errorMessage = "Not an Integer. Please enter a non-negtive quanrity to order.";
+            break;
+        default:
+            errorMessage = "";
+            break;
+    }
+    return errorMessage;
+}
+
+function displayPurchase() {
+    let quantity = Number(document.getElementById('qty_textbox').value);
+
+    let validationMessage = validateQuantity(quantity);
+
+    if(validationMessage == "") {
+        let message = `Thank you for ordering ${quantity} things!`;
+        document.body.innerHTML = message;
+    } else {
+        alert(validationMessage + "Please enter a positive integer for quantity.")
+        document.getElementById('qty_textbox').value="";
+    }
 }
